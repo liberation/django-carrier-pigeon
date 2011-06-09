7ee4968f9dbe0afb1e8424536a0b6d812098fda2
Amirouche Boubekki <amiroucheb@pilotsystems.net>
2011-06-09 16:32:05 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='django-push-content',
+      version='0.1',
+      description='Django application that help pushing content to remote locations',
+      author='Amirouche Boubekki',
+      author_email='amirouche.boubekki@gmail.com',
+      url='https://github.com/liberation/django-push-content',
+      packages=['distutils', 'distutils.command'],
+     )
