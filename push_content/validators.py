27805ed91b8b80cf0375ad4bc7ac6ba7d06c23c5
Yohan Boniface <y.boniface@liberation.fr>
2011-06-10 20:44:36 +0200

    Add wellformed_xml_validator

---
+++ push_content/validators.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+"""
+Validators are made to validate ouput. Validators must raise Exception if the 
+output do not validate.
+"""
+
+from xml.parsers.expat import ParserCreate
+
+
+def wellformed_xml_validator(output):
+    """
+    This check only the well-formedness.
+    It's not a DTD based validation.
+    """
+    parser = ParserCreate()
+    # parser will raise an Exception if the output is not wellformed
+    parser.Parse(output)
+    return True
