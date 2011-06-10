e1b34230ca6b54c9c71536d2ee372f370f64aaa1
Amirouche Boubekki <amiroucheb@pilotsystems.net>
2011-06-10 17:19:01 +0200

    added an abstract base class for configuration

---
+++ push_content/configuration.py
@@ -0,0 +1,28 @@
+class DefaultConfiguration:
+
+    @property
+    def push_urls(self):
+        raise NotImplementedError()
+
+    @property
+    def validators(self):
+        raise NotImplementedError()
+
+    def filter_by_instance_type(self, instance):
+        raise NotImplementedError()
+
+    def filter_by_updates(self, instance):
+        raise NotImplementedError()
+
+    def filter_by_state(self, instance):
+        raise NotImplementedError()
+
+    def get_directory(self, instance):
+        raise NotImplementedError()
+    
+    def get_extra_context(self, instance):
+        return dict()
+
+    def get_output_filename(self, instance):
+01        return '%s_%s.xml' % (instance._meta.app_label.lower(), 
+                              instance._meta.module_name)
