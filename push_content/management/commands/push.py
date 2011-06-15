2c553186996674f0f6428f5bd7f2af57e10b4bf3
Amirouche Boubekki <amiroucheb@pilotsystems.net>
2011-06-15 14:19:17 +0200

    push target_url is computed before push now (see previous commit for modification in models.py et select.py)

--- push_content/management/commands/push.py
+++ push_content/management/commands/push.py
@@ -9,14 +9,21 @@ from django.conf import settings
 from django.core.management.base import BaseCommand
 
 from push_content.models import ItemToPush
+
 from push_content.pusher import send
+
 from push_content.utils import URL
+from push_content.utils import join_url_to_directory
+
 from push_content import REGISTRY
+
 
 logger = logging.getLogger('push_content.command.push')
 
 
 def get_first_row_in_queue():
+    """Get the first row in the queue that 
+    can be processed"""
     try:
         row = ItemToPush.objects.all()
         row = row.order_by('creation_date')
@@ -33,26 +40,25 @@ class Command(BaseCommand):
     help = __doc__
 
     def handle(self, *args, **options):
-
         row = get_first_row_in_queue()
 
         if not row:
             return  # there is no more rows to push
 
         while True:
-            logger.debug('processing row id=%s, push=%s' %
-                         (row.id, row.target_url))
+            logger.debug('processing row id=%s, rule_name=%s' %
+                         (row.id, row.rule_name))
             row.status = ItemToPush.STATUS.IN_PROGRESS
             row.save()
 
             rule_name = row.rule_name
-            configuration = REGISTRY[rule_name]
+            rule = REGISTRY[rule_name]
 
             instance = row.content_object
 
             # build output
             try:
-                output = configuration.output(instance)
+                output = rule.output(instance)
             except Exception, e:
                 message = 'Exception ``%s`` raised: %s ' % (
                     e.__class__.__name__, e.message)
@@ -69,7 +75,7 @@ class Command(BaseCommand):
 
             # validate output
             validation = True
-            for validator in configuration.validators:
+            for validator in rule.validators:
                 try:
                     validator(output)
                     logger.debug('validation ``%s`` passed successfully'
@@ -78,7 +84,7 @@ class Command(BaseCommand):
                     validation = False
                     logger.debug('validation ``%s`` failed !')
                     message = 'catched exception %s : %s' % (
-                        Exception.__class__.__name__, e.message)
+                        e.__class__.__name__, e.message)
                     logger.debug(message)
                     row.status = ItemToPush.STATUS.VALIDATION_ERROR
                     if row.message != None:
@@ -87,21 +93,18 @@ class Command(BaseCommand):
                         row.message = message
                     row.save()
 
-            if not validation:  # if one validator did not pass we 
+            if not validation:  # if one validator did not pass we
                                 # do no want to send the file
                 # setting up next loop iteration
                 row = get_first_row_in_queue()
                 if not row:
                     return
                 continue
-            output_filename = configuration.get_output_filename(instance)
-
-            target_url = URL(row.target_url)
+            output_filename = rule.get_output_filename(instance)
 
             # build output file path for archiving
             output_directory = settings.PUSH_CONTENT_OUTPUT_DIRECTORY
-            output_directory += '/%s/%s' % (rule_name,
-                                            target_url.path)
+            output_directory += '/%s/' % rule_name
 
             # create output_directory if it doesn't exists
             if not os.path.exists(output_directory):
@@ -113,21 +116,39 @@ class Command(BaseCommand):
             f.write(output)
             f.close()
 
-            # try to send
-            max_ = settings.PUSH_CONTENT_MAX_PUSH_ATTEMPS
-            for push_attempt_num in xrange(max_):
-                logger.debug('push attempt %s' % push_attempt_num)
-                row.push_attempts += 1
-                row.last_push_attempts_date = datetime.now()
+            target_directory = None
+            try:
+                target_directory = row.get_directory(instance)
+            except Exception, e:
+                logger.error('error during ``get_directory``')
+                logger.error('catched exception message: %s' % e.message)
+                row = ItemToPush(rule_name=rule_name,
+                                 content_object=instance)
+                row.status = ItemToPush.STATUS.GET_DIRECTORY_ERROR
+                row.message = "%s: %s" % (e.__class__.__name__, e.message)
                 row.save()
+                return None
 
-                if send(row, target_url, output_path):
-                    row.status = ItemToPush.STATUS.PUSHED
+            for push_url in rule.push_urls:
+                target_url = join_url_to_directory(push_url, target_directory)
+                target_url = URL(target_url)
+
+
+                # try to send
+                max_ = settings.PUSH_CONTENT_MAX_PUSH_ATTEMPS
+                for push_attempt_num in xrange(max_):
+                    logger.debug('push attempt %s' % push_attempt_num)
+                    row.push_attempts += 1
+                    row.last_push_attempts_date = datetime.now()
                     row.save()
-                    logger.debug('succeeded')
-                    break  # send succeded, exit the for-block
-                else:
-                    logger.error('send failed')
+
+                    if send(row, target_url, output_path):
+                        row.status = ItemToPush.STATUS.PUSHED
+                        row.save()
+                        logger.debug('succeeded')
+                        break  # send succeded, exit the for-block
+                    else:
+                        logger.error('send failed')
 
             # try to fetch a new row
             row = get_first_row_in_queue()
