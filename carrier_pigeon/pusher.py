bb2ff365e8adca36c48868d4342fa09836f9d686
Yohan Boniface <y.boniface@liberation.fr>
2011-06-16 17:51:26 +0200

    FTP send : create remote dir if it does not exist

--- carrier_pigeon/pusher.py
+++ carrier_pigeon/pusher.py
@@ -1,6 +1,6 @@
 import os
 import logging
-from ftplib import FTP
+from ftplib import FTP, error_perm
 
 from carrier_pigeon.models import ItemToPush
 
@@ -17,8 +17,23 @@ def ftp_send(row, url, file_path):
             ftp.login(url.login, url.password)
         else:
             url.login()
-
-        ftp.cwd(url.path)
+        
+        # Path should not end with a trailing /
+        path = url.path
+        if path.endswith("/"):
+            path = url.path[:-1]
+        # Go to remote directory (create it if needed)
+        for directory in path.split("/"):
+            try:
+                ftp.cwd(directory)
+                # permanent error, in case the directory does not exist
+                # ftplib raise a "generic" error_perm
+            except error_perm, e:
+                ftp.mkd(directory)
+                # Don't catch the error now, in case the error_perm was for
+                # another reason
+                ftp.cwd(directory)
+            
         filename = os.path.split(file_path)[1]
         f = open(file_path)
         ftp.storbinary('STOR %s' % filename, f)
@@ -28,8 +43,7 @@ def ftp_send(row, url, file_path):
         return True
     except Exception, e:
         row.status = ItemToPush.STATUS.SEND_ERROR
-        row.message = '%s, exception message: %s' % ('ftp_send: ',
-                                                      e.message)
+        row.message = 'ftp_senf: exception message: %s' % (e.message)
         row.save()
         return False
 
