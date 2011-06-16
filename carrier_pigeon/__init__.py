54c0279713fe51f525cd0d6a7248a4ee1020d32f
Amirouche Boubekki <amiroucheb@pilotsystems.net>
2011-06-16 10:47:32 +0200

    post_save registring is done in class_prepared signal

--- carrier_pigeon/__init__.py
+++ carrier_pigeon/__init__.py
@@ -2,7 +2,7 @@ import logging
 
 from django.db import models as django_models
 from django.db.models.signals import post_save
-
+from django.db.models.signals import class_prepared
 
 REGISTRY = {}
 
@@ -20,28 +20,11 @@ def register(clazz):
     REGISTRY[name] = clazz()
 
 
-def load_models():
-    """Returns models from installed apps that inherits
-    BasicDirtyFieldsMixin"""
-    logger = logging.getLogger('carrier_pigeon.init')
-    logger.info('Start of configuration')
-    logger.debug('Seeking models that inherits BasicDirtyFieldsMixin.')
-    # compute classes that we want to catch
-    all_class = []
+def subscribe_to_post_save(sender, **kwargs):
+    if BasicDirtyFieldsMixin in sender.mro():
+        logger = logging.getLogger('carrier_pigeon.init')
+        msg = 'Subscribing post_save for %s model' % sender._meta.object_name
+        logger.debug(msg)
+        post_save.connect(select, sender=sender)
 
-    for app in django_models.get_apps():
-        klasses = django_models.get_models(app)
-        all_class.extend(klasses)
-
-    selected_models = [klass for klass in all_class
-                       if BasicDirtyFieldsMixin in klass.mro()]
-
-    return selected_models
-
-SELECTED_MODELS = load_models()
-
-for model in SELECTED_MODELS:
-    logger = logging.getLogger('carrier_pigeon.init')
-    msg = 'Subscribing post_save for %s model' % model._meta.object_name
-    logger.debug(msg)
-    post_save.connect(select, sender=model)
+class_prepared.connect(subscribe_to_post_save)
