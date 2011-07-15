cc5d974a213c7f3885c7971186e896ebae6777aa
Amirouche Boubekki <amiroucheb@pilotsystems.net>
2011-07-15 12:20:22 +0200

    added two settings to configure pigeon_check command

--- carrier_pigeon/management/commands/pigeon_check.py
+++ carrier_pigeon/management/commands/pigeon_check.py
@@ -5,29 +5,33 @@ from datetime import datetime
 from datetime import timedelta
 
 from django.core.management.base import BaseCommand
-
+from django.conf import settings
 from carrier_pigeon.models import ItemToPush
 
 
 class Command(BaseCommand):
     """Carrier pigeon health check
 
-    it returns exit code 0 if everything is OK.
-    it returns exit code *2* if there are new items 30 minutes old or more
-    it returns exit code *1* if there are new items 10 minutes old or more but
-    less that 30 minutes"""
+    - it returns exit code 0 if everything is OK.
+
+    - it returns exit code *2* if there are new items 
+      ``CARRIER_PIGEON_CHECK_OLD_AGE`` seconds old or more
+    
+    - it returns exit code *1* if there are new items 
+      ``CARRIER_PIGEON_CHECK_TOO_OLD_AGE`` seconds old or more but
+      less that 30 minutes"""
     help = __doc__
 
     def handle(self, *args, **options):
         now = datetime.now()
 
-        min30 = timedelta(seconds=60*30) # 30 minutes
+        min30 = timedelta(seconds=settings.CARRIER_PIGEON_CHECK_OLD_AGE)
         min30_count = ItemToPush.objects.filter(creation_date__lt=now-min30,
                                                 status=ItemToPush.STATUS.NEW).count()
         if min30_count > 0:
             sys.exit(2)
 
-        min10 = timedelta(seconds=60*10) # 10 minutes
+        min10 = timedelta(seconds=settings.CARRIER_PIGEON_CHECK_TOO_OLD_AGE)
         min10_count = ItemToPush.objects.filter(creation_date__lt=now-min10,
                                                 status=ItemToPush.STATUS.NEW).count()
         if min10_count > 0:
