d373f65955bb6fbdeda73c298d34dbf9d509238b
Amirouche Boubekki <amiroucheb@pilotsystems.net>
2011-07-21 17:26:58 +0200

    debug setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from distutils.core import setup
 setup(name='django-carrier-pigeon',
       version='0.1',
       description='Django application that help pushing content to remote locations',
-      author='Amirouche Boubekki',
-      author_email='amirouche.boubekki@gmail.com',
+      author='Djaz Team',
+      author_email='devweb@liberation.fr',
       url='https://github.com/liberation/django-push-content',
-      packages=['distutils', 'distutils.command'],
+      packages=['carrier_pigeon'],
      )
