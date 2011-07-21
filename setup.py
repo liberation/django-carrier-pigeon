6ee28a73c7f27e62d863ed419b3eb84ab47c5582
Amirouche Boubekki <amiroucheb@pilotsystems.net>
2011-07-21 17:37:38 +0200

    better setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
-
+from os import path
 from distutils.core import setup
+
+with open(path.join(path.dirname(__file__), 'README.rst')) as f:
+    readmin = f.read()
 
 setup(name='django-carrier-pigeon',
       version='0.1',
       description='Django application that help pushing content to remote locations',
+      long_description=readme,
       author='Djaz Team',
       author_email='devweb@liberation.fr',
       url='https://github.com/liberation/django-push-content',
