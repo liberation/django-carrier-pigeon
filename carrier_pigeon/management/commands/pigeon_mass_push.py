35813d8caea9d42812c25c062770bfc615e3a197
Amirouche Boubekki <amiroucheb@pilotsystems.net>
2012-04-06 12:48:15 +0200

    pigeon_mass_push now takes a rule name as argument and a liste of arguments passed to rule.get_items_to_push this we can parametrize a push

--- carrier_pigeon/management/commands/pigeon_mass_push.py
+++ carrier_pigeon/management/commands/pigeon_mass_push.py
@@ -12,19 +12,18 @@ logger = logging.getLogger('carrier_pigeon.command.mass_push')
 
 class Command(BaseCommand):
     """ Archive and push content items as specified. """
-    args = '<rule_name rule_name ...>'
+    args = '<rule_name get item to push arguments>'
     help = __doc__
 
     def handle(self, *args, **options):
-        for rule_name in args:
-            try:
-                rule = REGISTRY[rule_name]
-            except KeyError:
-                logger.warning(u"Sorry, rule '%s' does not exist" % rule_name)
-                continue
-            
+        rule_name = args[0]
+        try:
+            rule = REGISTRY[rule_name]
+        except KeyError:
+            logger.warning(u"Sorry, rule '%s' does not exist" % rule_name)
+        else:
             # --- EXPORT ALL THE THINGS! \o/
             rule.initialize_push()
-            for item in rule.get_items_to_push():
+            for item in rule.get_items_to_push(*args[1:]):
                 rule.process_item(item)
             rule.finalize_push()
